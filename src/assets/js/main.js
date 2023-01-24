28b2ca8fa05a52df4bc22ce41fb5f60b5a2d81a3
FabriciosViana <vianasfabricio@gmail.com>
2023-01-24 09:52:19 -0300

    Deploy

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+
+const nav = document.querySelector('.nav')
+
+nav.addEventListener('click', () =>{
+    nav.classList.toggle('active')
+})
+
+const target = document.querySelectorAll('[data-anime]')
+
+const animationClass = 'animate'
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + (window.innerHeight * 3 / 4)
+    target.forEach(function(element){
+        if(windowTop > element.offsetTop){
+            element.classList.add(animationClass)
+        }else{
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll()
+})
